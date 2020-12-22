96a454fff912b4947644e4b92331b91cb15d3757
Rob Wallator <rob17macbook@MacbookPro17.local>
2020-12-22 15:35:17 -0700

    deleted firebase/app from firebase index

--- src/firebase/firebaseIndex.js
+++ src/firebase/firebaseIndex.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import "firebase/app";
+//import "firebase/app";
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
